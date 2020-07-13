f331afe5e59a3051d958632e81c98fcd289297e2
STORYM <ssashkaa01@gmail.com>
2020-07-13 12:50:45 +0300

    add hw

---
+++ 23.07.2020/WebExams/App_Start/RouteConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebExams
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}",
+                defaults: new { controller = "Account", action = "Login" }
+            );
+
+            routes.MapRoute(
+                name: "Register",
+                url: "{controller}/{action}",
+                defaults: new { controller = "Account", action = "Register" }
+            );
+
+            routes.MapRoute(
+                name: "Profile",
+                url: "{controller}/{action}",
+                defaults: new { controller = "UserProfile", action = "Profile" }
+            );
+        }
+    }
+}
